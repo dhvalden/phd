6b69b9e2bfa2a8bec4e9ce225af83105344d68d9
Daniel Valdenegro <dhvalden@gmail.com>
2020-04-12 00:14:21 +0100

    fix error. No more full_text()

--- digester/ingester.py
+++ digester/ingester.py
@@ -43,25 +43,6 @@ def rawgencount(filename):
     sys.stdout.write("%s objects found. " % nlines)
 
 
-def extract_text(tweet):
-    full_text = "".encode()
-    if "retweeted_status" in tweet:
-        if "extended_tweet" in tweet["retweeted_status"]:
-            full_text = tweet["retweeted_status"]["extended_tweet"]["full_text"]
-        else:
-            full_text = tweet["retweeted_status"]["text"]
-    elif "quoted_status" in tweet:
-        if "extended_tweet" in tweet["quoted_status"]:
-            full_text = tweet["quoted_status"]["extended_tweet"]["full_text"]
-        else:
-            full_text = tweet["quoted_status"]["text"]
-    elif "extended_tweet" in tweet:
-        full_text = tweet["extended_tweet"]["full_text"]
-    else:
-        full_text = tweet["text"]
-    return(full_text)
-
-
 def ingester(line):
     tweet = json.loads(line)
     digest = {}
@@ -82,12 +63,15 @@ def ingester(line):
     digest["reply_count"] = tweet["reply_count"]
     digest["retweet_count"] = tweet["retweet_count"]
     digest["favorite_count"] = tweet["favorite_count"]
+    digest["retweeted_status"]["text"] = tweet.get("retweeted_status").get("text")
+    digest["quoted_status"]["text"] = tweet.get("quoted_status").get("text")
+    digest["retweeted_status"]["full_text"] = tweet.get("retweeted_status").get("extended_tweet").get("full_text")
+    digest["quoted_status"]["full_text"] = tweet.get("quoted_status").get("extended_tweet").get("full_text")
     digest["hashtags"] = tweet["entities"]["hashtags"]
     digest["urls"] = tweet["entities"]["urls"]
     digest["user_mentions"] = tweet["entities"]["user_mentions"]
-    digest["full_text"] = extract_text(tweet)
     digest["text"] = tweet["text"]
-    digest["extended_text"] = tweet.get("extended_tweet", {}).get("full_text")
+    digest["extended_text"] = tweet.get("extended_tweet").get("full_text")
     digest["source"] = tweet["source"]
     digest["lang"] = tweet["lang"]
 
