de2c813979ea16c16c76cef539900a2a6c2a97ab
Daniel Valdenegro <dhvalden@gmail.com>
2020-04-28 01:22:40 +0100

    adding fast look up for tweets with emojis

---
+++ emoji_extraction/src/lib.rs
@@ -0,0 +1,98 @@
+use serde::{Serialize, Deserialize};
+use std::error::Error;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
+use aho_corasick::AhoCorasick;
+
+pub struct Config {
+    pub infile: String,
+    pub outfile: String,
+    pub query: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Input {
+    id: Option<String>,
+    created_at: Option<String>,
+    user_id: Option<String>,
+    text: String,
+    extended_text: Option<String>,
+    lang: String,
+}
+
+impl Input {
+    fn full_text(&self) -> String {
+        match &self.extended_text {
+            None => self.text.clone(),
+            Some(x) => x.to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct Output {
+    id: Option<String>,
+    created_at: Option<String>,
+    user_id: Option<String>,
+    full_text: String,
+    lang: String,
+}
+
+impl From<Input> for Output {
+    fn from(item: Input) -> Output {
+        Output {
+            full_text: item.full_text().to_string(),
+            id: item.id,
+            created_at: item.created_at,
+            user_id: item.user_id,
+            lang: item.lang,
+        }
+    }
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let infile = args[1].clone();
+        let outfile = args[2].clone();
+        let query = args[3].clone();
+
+        Ok(Config { infile, outfile, query })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let queryfile = File::open(config.query)?;
+    let queryreader = BufReader::new(queryfile);
+    let patterns: Vec<String> = queryreader
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+    let ac = AhoCorasick::new(patterns);
+
+    let infile = File::open(config.infile)?;
+    let reader = BufReader::new(infile);
+    let mut outfile = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(config.outfile)
+        .unwrap();
+
+    for line in reader.lines() {
+        let v: Input = serde_json::from_str(&line.unwrap())?;
+        if (&v.lang == "en") && (ac.is_match(&v.text)) {
+            let output = Output::from(v);
+            let j = serde_json::to_string(&output)?;
+            if let Err(e) = writeln!(outfile, "{}", &j) {
+                eprintln!("Couldn't write to file: {}", e);
+            }
+            println!("{}", j);
+        }
+    }
+    Ok(())
+}
---
+++ emoji_extraction/src/main.rs
@@ -0,0 +1,21 @@
+use emoji_extraction::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Processing {}", config.infile);
+    println!("Output file {}", config.outfile);
+
+    if let Err(e) = emoji_extraction::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
