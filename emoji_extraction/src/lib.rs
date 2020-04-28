8918b6f61dbfdfc16f18bd42408987e930850b91
Daniel Valdenegro <dhvalden@gmail.com>
2020-04-28 16:54:18 +0100

    adding fast look up for tweets with emojis

--- emoji_extraction/src/lib.rs
+++ emoji_extraction/src/lib.rs
@@ -17,16 +17,16 @@ pub struct Input {
     id: Option<String>,
     created_at: Option<String>,
     user_id: Option<String>,
-    text: String,
+    text: Option<String>,
     extended_text: Option<String>,
-    lang: String,
+    lang: Option<String>,
 }
 
 impl Input {
-    fn full_text(&self) -> String {
+    fn full_text(&self) -> Option<String> {
         match &self.extended_text {
             None => self.text.clone(),
-            Some(x) => x.to_string(),
+            Some(x) => Some(x.to_string()),
         }
     }
 }
@@ -36,14 +36,14 @@ pub struct Output {
     id: Option<String>,
     created_at: Option<String>,
     user_id: Option<String>,
-    full_text: String,
-    lang: String,
+    full_text: Option<String>,
+    lang: Option<String>,
 }
 
 impl From<Input> for Output {
     fn from(item: Input) -> Output {
         Output {
-            full_text: item.full_text().to_string(),
+            full_text: item.full_text(),
             id: item.id,
             created_at: item.created_at,
             user_id: item.user_id,
@@ -85,7 +85,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     for line in reader.lines() {
         let v: Input = serde_json::from_str(&line.unwrap())?;
-        if (&v.lang == "en") && (ac.is_match(&v.text)) {
+        if (&v.lang.as_ref().unwrap().to_string() == "en") && (ac.is_match(&v.text.as_ref().unwrap())) {
             let output = Output::from(v);
             let j = serde_json::to_string(&output)?;
             if let Err(e) = writeln!(outfile, "{}", &j) {
